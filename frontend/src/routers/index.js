3e5d4c4aaf7e290f2d863478ecca5a5660c6b65f
Anh Duc <anhduc30122003@gmail.com>
2025-02-23 13:53:10 +0700

    homepage_aboutus.1.0

--- frontend/src/routers/index.js
+++ frontend/src/routers/index.js
@@ -2,6 +2,8 @@ import NewsPage from "../pages/NewsPage";
 import ServicePage from "../pages/ServicePage";
 import CustomerPage from "../pages/CustomerPage";
 import OfferPage from "../pages/OfferPage";
+import HomePage from "../pages/HomePage";
+import AboutUs from "../pages/AboutUsPage";
 
 export const routes = [
   {
@@ -22,6 +24,16 @@ export const routes = [
   {
     path: "/offer",
     page: OfferPage,
+    isShowHeader: true,
+  },
+  {
+    path: "/home",
+    page: HomePage,
+    isShowHeader: true,
+  },
+  {
+    path: "/us",
+    page: AboutUs,
     isShowHeader: true,
   },
 ];
